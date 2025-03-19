81dbec62e70a12ab7884c4cbf62c3db5e983acd9
jongdu <parksn5029@naver.com>
2025-03-19 17:22:16 +0900

    [Feat] 주문 취소 로직 수정 완료

--- order-service/src/main/java/com/logi_manage/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/logi_manage/order_service/controller/OrderController.java
@@ -3,7 +3,9 @@ package com.logi_manage.order_service.controller;
 import com.logi_manage.order_service.dto.request.CreateOrderRequestDto;
 import com.logi_manage.order_service.dto.request.OrderFilterRequestDto;
 import com.logi_manage.order_service.dto.request.UpdateOrderStatusRequestDto;
+import com.logi_manage.order_service.dto.response.DeleteOrderResponseDto;
 import com.logi_manage.order_service.dto.response.OrderDetailResponseDto;
+import com.logi_manage.order_service.dto.response.OrderItemsStatusResponseDto;
 import com.logi_manage.order_service.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Slf4j
@@ -29,6 +33,7 @@ public class OrderController {
 
     /**
      * 신규 주문 생성
+     *
      * @param createOrderRequestDto 주문 생성 dto
      * @return 생성된 주문 id
      */
@@ -40,7 +45,8 @@ public class OrderController {
 
     /**
      * 주문 상태 변경
-     * @param orderId 수정할 주문 id
+     *
+     * @param orderId                     수정할 주문 id
      * @param updateOrderStatusRequestDto 주문 변경 dto
      */
     @PostMapping("/{orderId}")
@@ -51,28 +57,32 @@ public class OrderController {
 
     /**
      * 주문 취소
+     *
      * @param orderId 취소할 주문 id
+     * @return 주문 취소 info list
      */
     @DeleteMapping("/{orderId}")
-    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
-        orderService.deleteOrder(orderId);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<DeleteOrderResponseDto> deleteOrder(@PathVariable Long orderId) {
+        DeleteOrderResponseDto deleteOrderResponseDto = orderService.deleteOrder(orderId);
+        return ResponseEntity.ok(deleteOrderResponseDto);
     }
 
     /**
      * 주문 리스트 조회
+     *
      * @param filterRequestDto 필터링 dto
-     * @param pageable 페이징
+     * @param pageable         페이징
      * @return 주문 list
      */
     @GetMapping
-    public ResponseEntity<?> getOrderList(OrderFilterRequestDto filterRequestDto, Pageable pageable) {
+    public ResponseEntity<Page<OrderDetailResponseDto>> getOrderList(OrderFilterRequestDto filterRequestDto, Pageable pageable) {
         Page<OrderDetailResponseDto> orderList = orderService.getOrderList(filterRequestDto, pageable);
         return ResponseEntity.ok(orderList);
     }
 
     /**
      * 주문 상세 조회
+     *
      * @param orderId 조회할 주문 id
      * @return 주문 상세 info
      */
